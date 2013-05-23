d682400901c0853b1b5c6b620b5211cb191b8d69
derigel <derigel@mail.ru>
2013-05-23 12:22:18 +0400

    Baseline plugin stub

---
+++ JetBox/Properties/AssemblyInfo.cs
@@ -0,0 +1,22 @@
+using System.Reflection;
+using JetBrains.Application.PluginSupport;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("JetBox")]
+[assembly: AssemblyDescription("ReSharper settings synchronization through DropBox")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Slava Trenogin")]
+[assembly: AssemblyProduct("JetBox")]
+[assembly: AssemblyCopyright("Copyright © Slava Trenogin, 2013")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: AssemblyVersion("0.1.0.0")]
+[assembly: AssemblyFileVersion("0.1.0.0")]
+
+// The following information is displayed by ReSharper in the Plugins dialog
+[assembly: PluginTitle("JetBox")]
+[assembly: PluginDescription("ReSharper settings synchronization through DropBox")]
+[assembly: PluginVendor("Slava Trenogin")]
