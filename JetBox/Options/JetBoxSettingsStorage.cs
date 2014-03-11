dad6f13c2bb08aa1ab9ee994dbfe0205e23c3a77
derigel <derigel@mail.ru>
2014-03-11 15:54:50 +0400

    ReSharper 8.2 support

--- JetBox/Options/JetBoxSettingsStorage.cs
+++ JetBox/Options/JetBoxSettingsStorage.cs
@@ -26,16 +26,35 @@ namespace JetBox.Options
 
     private class JetBoxSettingsProvider : FileSettingsStorageProviderBase
     {
-      public JetBoxSettingsProvider([NotNull] Lifetime lifetime, [NotNull] string name, [NotNull] IProperty<FileSystemPath> path, bool isWritable, double priority, [NotNull] IIsAvailable isAvailable, SettingsStoreSerializationToXmlDiskFile.SavingEmptyContent whenNoContent, [NotNull] IThreading threading, [NotNull] IFileSystemTracker filetracker, [NotNull] IFileSettingsStorageBehavior behavior, InternKeyPathComponent interned, IEnumerable<KeyValuePair<PropertyId, object>> metadata)
-        : base(lifetime, name, path, isWritable, priority, isAvailable, whenNoContent, threading, filetracker, behavior, interned, metadata)
+      public JetBoxSettingsProvider([NotNull] Lifetime lifetime, [NotNull] string name, [NotNull] IProperty<FileSystemPath> path, bool isWritable, double priority, [NotNull] IIsAvailable isAvailable, SettingsStoreSerializationToXmlDiskFile.SavingEmptyContent whenNoContent, [NotNull] IThreading threading, [NotNull] IFileSystemTracker filetracker,
+#if RS_80
+        [NotNull] FileSettingsStorageBehavior behavior,
+#else
+        [NotNull] IFileSettingsStorageBehavior behavior, InternKeyPathComponent interned,
+#endif
+        IEnumerable<KeyValuePair<PropertyId, object>> metadata)
+        : base(lifetime, name, path, isWritable, priority, isAvailable, whenNoContent, threading, filetracker, behavior, 
+#if RS_80
+#else
+                interned,
+#endif
+        metadata)
       {}
     }
-
+#if RS_80
+    public JetBoxSettingsStorage(Lifetime lifetime, ProductSettingsLocation productSettingsLocation, ISettingsSchema settingsSchema, DataContexts dataContexts, IThreading threading, IFileSystemTracker fileSystemTracker, FileSettingsStorageBehavior settingsStorageBehavior, ISettingsLogger settingsLogger, ISettingsChangeDispatch settingsChangeDispatch, SettingsStorageMountPoints.SelfCheckControl selfCheckControl)
+#else
     public JetBoxSettingsStorage(Lifetime lifetime, ProductSettingsLocation productSettingsLocation, ISettingsSchema settingsSchema, DataContexts dataContexts, IThreading threading, IFileSystemTracker fileSystemTracker, IFileSettingsStorageBehavior settingsStorageBehavior, ISettingsLogger settingsLogger, ISettingsChangeDispatch settingsChangeDispatch, SettingsStorageMountPoints.SelfCheckControl selfCheckControl, InternKeyPathComponent interned)
+#endif
     {
       var filePath = productSettingsLocation.GetUserSettingsNonRoamingDir(ProductSettingsLocationFlag.ThisProductThisVersionThisEnvironment).Combine("JetBox" + XmlFileSettingsStorage.SettingsStorageFileExtensionWithDot);
       var property = new Property<FileSystemPath>(lifetime, GetType().Name + "Path", filePath);
-      var settingsProvider = new JetBoxSettingsProvider(lifetime, GetType().Name + "::Provider", property, true, 0, IsAvailable.Always, SettingsStoreSerializationToXmlDiskFile.SavingEmptyContent.DeleteFile, threading, fileSystemTracker, settingsStorageBehavior, interned, new Dictionary<PropertyId, object>());
+      var settingsProvider = new JetBoxSettingsProvider(lifetime, GetType().Name + "::Provider", property, true, 0, IsAvailable.Always, SettingsStoreSerializationToXmlDiskFile.SavingEmptyContent.DeleteFile, threading, fileSystemTracker, settingsStorageBehavior,
+#if RS_80
+#else
+                interned,
+#endif
+      new Dictionary<PropertyId, object>());
       var mounts = new SettingsStorageMountPoints(lifetime,
         new CollectionEvents<IProvider<ISettingsStorageMountPoint>>(lifetime, GetType() + "::Mounts") { settingsProvider }, threading, settingsLogger,
         selfCheckControl);
